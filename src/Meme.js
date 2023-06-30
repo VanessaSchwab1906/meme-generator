51cfb477d1fa23a15744129ca7b814e6e8fb36ab
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-06-30 11:48:11 +0200

    meme generating function

--- src/Meme.js
+++ src/Meme.js
@@ -1,7 +1,40 @@
 import React from "react";
 import "./style.css";
+import { useState, useEffect } from "react";
 
 function Meme(props) {
+  const [appData, setAppData] = useState({
+    firstline: "",
+    secondline: "",
+  });
+  // const [newMemeData, setNewMemeData] = useState([]);
+  const [memeList, setmemeList] = useState([]);
+  const [randomMeme, setrandomMeme] = useState(null);
+  function meme() {
+    fetch("http://localhost:1337/api/memes?populate=meme")
+      .then((res) => res.json())
+      .then((meme) => {
+        setmemeList(meme.data);
+      });
+  }
+  useEffect(() => {
+    meme();
+  }, []);
+  function getRandomMeme() {
+    const randomNumber = Math.floor(Math.random() * memeList.length);
+    let randomMemeURL =
+      memeList[randomNumber].attributes.meme.data.attributes.url;
+    setrandomMeme(randomMemeURL);
+  }
+  function enterLine(event) {
+    setAppData((prevAppData) => {
+      return {
+        ...prevAppData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+  console.log(appData);
   return (
     <div className="meme-container">
       <nav className="navbar">
@@ -13,16 +46,32 @@ function Meme(props) {
           type="text"
           placeholder="Enter the first line..."
           name="firstline"
+          onChange={enterLine}
+          value={appData.firstline}
         />
         <input
           type="text"
           placeholder="Enter the second line..."
           name="secondline"
+          onChange={enterLine}
+          value={appData.secondline}
         />
       </div>
-      <button className="generateBTN">Load random image</button>
+      <button className="generateBTN" onClick={getRandomMeme}>
+        Load random image
+      </button>
+      {randomMeme !== null && (
+        <div className="meme-image">
+          <img
+            className="imageMeme"
+            src={`http://localhost:1337${randomMeme}`}
+            alt="Meme Not Responding "
+          />
+          <h2 className="first">{appData.firstline}</h2>
+          <h2 className="second">{appData.secondline}</h2>
+        </div>
+      )}
     </div>
   );
 }
-
 export default Meme;
